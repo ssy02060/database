4c80077d71de194357c39a065410049a09720a1a
ssy <ssy02060@gmail.com>
2019-10-07 10:34:52 +0900

    10/7 excuteScalar() 실습

---
+++ DB/DB/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Oracle.DataAccess.Client.OracleParameter oracleParameter2 = new Oracle.DataAccess.Client.OracleParameter();
+            this.oracleCommand1 = new Oracle.DataAccess.Client.OracleCommand();
+            this.oracleConnection1 = new Oracle.DataAccess.Client.OracleConnection();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.oracleCommand2 = new Oracle.DataAccess.Client.OracleCommand();
+            this.serch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.oracleCommand3 = new Oracle.DataAccess.Client.OracleCommand();
+            this.SuspendLayout();
+            // 
+            // oracleCommand1
+            // 
+            this.oracleCommand1.CommandText = "SELECT * FROM EMP";
+            this.oracleCommand1.Connection = this.oracleConnection1;
+            this.oracleCommand1.Transaction = null;
+            // 
+            // oracleConnection1
+            // 
+            this.oracleConnection1.ConnectionString = "Data Source=ORARA;User ID=A5293638;Password=san9yun2;";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(21, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 196);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "불러오기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // oracleCommand2
+            // 
+            this.oracleCommand2.CommandText = "SELECT * FROM EMP";
+            this.oracleCommand2.Connection = this.oracleConnection1;
+            this.oracleCommand2.Transaction = null;
+            // 
+            // serch
+            // 
+            this.serch.Location = new System.Drawing.Point(197, 102);
+            this.serch.Name = "serch";
+            this.serch.Size = new System.Drawing.Size(75, 23);
+            this.serch.TabIndex = 3;
+            this.serch.Text = "검색";
+            this.serch.UseVisualStyleBackColor = true;
+            this.serch.Click += new System.EventHandler(this.serch_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(287, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // oracleCommand3
+            // 
+            this.oracleCommand3.CommandText = "SELECT * FROM EMP";
+            this.oracleCommand3.Connection = this.oracleConnection1;
+            oracleParameter2.OracleDbType = Oracle.DataAccess.Client.OracleDbType.Varchar2;
+            oracleParameter2.ParameterName = "pa";
+            this.oracleCommand3.Parameters.Add(oracleParameter2);
+            this.oracleCommand3.Transaction = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.serch);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Oracle.DataAccess.Client.OracleCommand oracleCommand1;
+        private Oracle.DataAccess.Client.OracleConnection oracleConnection1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Oracle.DataAccess.Client.OracleCommand oracleCommand2;
+        private System.Windows.Forms.Button serch;
+        private System.Windows.Forms.TextBox textBox1;
+        private Oracle.DataAccess.Client.OracleCommand oracleCommand3;
+    }
+}
+
---
+++ DB/DB/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            oracleCommand1.CommandText = "SELECT * FROM EMP";
+            oracleCommand2.CommandText = "SELECT ename FROM emp WHERE empno = '1'";
+            oracleCommand3.CommandText = "SELECT * FROM EMP WHERE EMPNO = :PARAM";
+            
+        }
+        //ExcuteReader(); select 반환값 여러개 가능
+        //ExcuteScalar(); select 반환값 한 개만 가능 (count와 같으 명령어)
+        //ExcuteNonQuery(); insert, Delete, update를 사용할때
+
+        //OracleDataReader : Cursor의 개념 첫번째 record 앞을 가리킨다. read 메서드를 호출할때마다 한개씩 읽어온다.
+        //데이터를 다읽어오면 False
+        //rdr 이라는 커서는 위에서 밑으로
+        //column의 위치를 정확하게 안다면 "rdr[숫자]"로 표현 가능
+
+           
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oracleConnection1.Open();
+            MessageBox.Show("연결성공");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OracleDataReader rdr = oracleCommand1.ExecuteReader();
+
+            /*int result = Convert.ToInt32(oracleCommand1.ExecuteScalar());
+            listBox1.Items.Add("전체 수 : " + result.ToString());
+            listBox1.Items.Add("empno = 1의 이름: " + oracleCommand2.ExecuteScalar());
+            */
+            int no1 = rdr.GetOrdinal("EMPNO");
+            int no2 = rdr.GetOrdinal("ENAME");
+            int no3 = rdr.GetOrdinal("JOB");
+
+            while (rdr.Read())
+            { 
+                listBox1.Items.Add(rdr[no1].ToString() + " " + rdr[no2].ToString() + " " + rdr[no3].ToString());
+            }
+            rdr.Close();
+            
+        }
+
+        private void serch_Click(object sender, EventArgs e)
+        {
+            oracleCommand3.Parameters[0].Value = textBox1.Text;
+            OracleDataReader rdr = oracleCommand3.ExecuteReader();
+            listBox1.Items.Clear();
+
+            while (rdr.Read())
+                listBox1.Items.Add(rdr["ENAME"]);
+            rdr.Close();
+        }
+    }
+}
